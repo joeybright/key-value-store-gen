9462d183411568716bd074999b2cbd98b12e6410
Joey Bright <hello@joeybright.com>
2022-12-06 18:19:48 -0800

    Removed indexedDB example

--- examples/indexedDB/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import elmPlugin from "vite-plugin-elm";
-
-export default defineConfig({
-    root: "src",
-    build: { outDir: "dist" },
-    plugins: [elmPlugin()],
-});
